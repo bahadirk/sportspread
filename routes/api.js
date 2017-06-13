d7ae651e215627f4254933ae5506139ed47a18c0
bahadirk <bahadir.kirdan@tum.de>
2017-06-14 01:27:58 +0200

    Added AngularJS with first initial page

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
 
 /* GET users listing. */
-router.get('/tasks', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  User.find(function(err, users){
+    if(err){
+      res.send(err);
+    }
+    res.send(users);
+  });
 });
+
+
+/* GET user with id. */
+router.get('/users/:id', function(req, res, next) {
+  var userId = req.params.id;
+
+  User.findById(userId, function(err, user){
+    if(err){
+      res.send(err);
+    }
+    res.send(user);
+  });
+});
+
+
 
 module.exports = router;
